b82e6ded8d5d0601312cafcfd7fed8fe2793ee3e
Tomas Roy <hello@minicreative.net>
2017-09-11 14:16:37 -0400

    server.js setup, npm init

---
+++ server.js
@@ -0,0 +1,17 @@
+
+// Initialize dependencies
+const express = require('express');
+
+// Initialize config
+const config = require('./config');
+
+// Setup server
+const server = express();
+
+server.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+server.listen(config.port, config.ip function () {
+  console.log('cChange API running on port 3000');
+})
