b8c25595a9a8f028f3febe0e7acde846abbc7404
Tomas Roy <hello@minicreative.net>
2017-09-17 17:24:24 -0400

    CORS headers

--- api/Routes.js
+++ api/Routes.js
@@ -2,6 +2,15 @@
 // Attach middleware and all routing files to server
 module.exports = function (server) {
 
+	// CORS Headers
+	app.use(function (req, res, next) {
+	    res.setHeader('Access-Control-Allow-Origin', '*');
+	    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	    res.setHeader('Access-Control-Allow-Credentials', true);
+	    next();
+	});
+
 	// Welcome
 	server.get('/', function (request, response, next) {
 		response.send('Welcome to the cChange API');
